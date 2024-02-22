46bcad5797e5ee1f59756b176b72dd719a32f34a
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-02-22 11:15:50 +0800

    feat: Customize logo size
    Add new props to Logo component to customize logo size

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/images/logo.svg';
-export default function Logo() {
+export default function Logo({ width, height }) {
 	return (
 		<nav>
 			<img
 				src={logo}
 				alt='TODOAPP'
 				className='logo'
-				style={{ margin: '0.2rem',  }}
+				style={{ margin: '0.2rem', width: `${width}px`, height: `${height}px` }}
 			/>
 		</nav>
 	);
